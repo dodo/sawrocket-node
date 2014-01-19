956af3fa739c4827466ea75c6557c7fee536e23b
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2014-01-19 19:47:55 +0100

    port tls to node 0.11.10 and use node-forge to do the actual TLS

--- tls.js
+++ tls.js
@@ -2,156 +2,159 @@
  * Simulate node's TLS wrapper using Forge
  * borrowed from https://github.com/hiddentao/browsermail
  * https://github.com/hiddentao/browsermail/blob/master/src/js/node-polyfills/tls.js
+ * and ported to nodejs api v0.11
  */
+var util = require('util');
 var EventEmitter = require('events').EventEmitter;
+var crypto = require('./crypto');
 
-var forge = require('forge');
+var CLIENT_TO_SERVER = 1;
+var SERVER_TO_CLIENT = 2;
+var debug = util.debuglog ? (function () {
+    var log = util.debuglog('tls');
+    return function (msg, direction, contentType) {
+      switch (direction) {
+        case CLIENT_TO_SERVER:
+          direction = '{c -> S}: ';
+          break;
+        case SERVER_TO_CLIENT:
+          direction = '{S -> c}: ';
+          break;
+        default:
+          direction = '';
+      }
+      msg = ('enc' !== contentType ? msg : '(enc) ' + crypto.forge.util.bytesToHex(msg));
+      log(direction + msg);
+    }
+})() : function () {};
 
 
-var TlsSocket = function(options) {
+
+function TLSSocket(socket, options) {
+  if (!options) {options = socket;socket = null}
   var self = this;
 
-  var CLIENT_TO_SERVER = 1;
-  var SERVER_TO_CLIENT = 2;
+  this._socket = socket || options.socket;
 
-  self._socket = options.socket;
+  // To prevent assertion in afterConnect()
+  if (this._socket)
+    this._connecting = this._socket._connecting;
 
-  self.id = self._socket.id;
-  self.writable = false;
+  this.id = this._socket._handle.socketId;
+  this._tlsOptions = options;
+  this.authorizationError = null;
+  this.authorized = false;
+  this.writable = false;
 
+  var ctx = (options.credentials || crypto.createCredentials()).context;
 
   // create TLS connection
-  self._tlsConnection = forge.tls.createConnection({
-    server: false,
-    sessionId: options.sessionId || null,
-    caStore: options.caStore || [],
-    sessionCache: options.sessionCache || null,
-    cipherSuites: options.cipherSuites || null,
-    virtualHost: options.virtualHost,
-    verify: options.verify,
-    getCertificate: options.getCertificate,
-    getPrivateKey: options.getPrivateKey,
-    getSignature: options.getSignature,
-    deflate: options.deflate,
-    inflate: options.inflate,
-    verify: function(conn, verified, depth, certs) {
-      return true;
-    },
-    connected: function(conn) {
-      options.debug && self._trace('Handshake successful');
-
-      // first handshake complete, call handler
-      if(conn.handshakes === 1) {
-        self.writable = true;
-        self.emit('connected');
-      }
-    },
-    tlsDataReady: function(conn) {
-      var bytes = conn.tlsData.getBytes();
-      options.debug && self._trace(bytes, CLIENT_TO_SERVER, 'enc');
-
-      // send TLS data over socket
-      self._socket.write(bytes, 'binary', function(err) {
-        if (err) {
-          self.emit('error', err);
-        }
-      });
-    },
-    dataReady: function(conn) {
-      var received = conn.data.getBytes();
-
-      options.debug && self._trace(received, SERVER_TO_CLIENT, 'plain');
-
-      // indicate application data is ready
-      self.emit('data', new Buffer(received, 'binary'));
-    },
-    closed: function() {
-      self.writable = false;
-    },
-    error: function(conn, e) {
-      options.debug && self._trace('Error: ' + e.message || e);
-
-      // send error, close socket
-      self.emit('error', e);
-      self._socket.close();
-    }
+  this.ssl = crypto.forge.tls.createConnection({
+    server: typeof options.isServer === 'undefined' ? false : options.isServer,
+    verifyClient: options.requestCert ? options.rejectUnauthorized ? true : 'optional' : false,
+    error: onError.bind(this),
+    closed: onClosed.bind(this),
+    connected: onConnected.bind(this),
+    dataReady: onDataReady.bind(this),
+    tlsDataReady: onTlsDataReady.bind(this),
+//     getCertificate: ctx.getCert.bind(ctx), // FIXME
+//     getPrivateKey: ctx.getKey.bind(ctx),  // FIXME
+//     getSignature: ctx.sign.bind(ctx),    // FIXME
+    deflate: ctx.deflate && ctx.deflate.bind(ctx),
+    inflate: ctx.inflate && ctx.inflate.bind(ctx),
+    sessionCache: ctx.session.cache,
+    cipherSuites: ctx.cipherSuites,
+    virtualHost: ctx.virtualHost,
+    sessionId: ctx.session.id,
+    caStore: ctx.caStore || [],
+    verify: ctx.verify && ctx.verify.bind(ctx) ||
+      function(conn, verified, depth, certs) {
+        return true; // FIXME
+      },
   });
 
-  // handle doing handshake after connecting
-  self._socket.once('connect', function() {
-    options.debug && self._trace('Socket connected. Handshaking...');
-
-    self._tlsConnection.handshake(options.sessionId);
-  });
-
-  self._socket.on('close', function(had_err) {
-    if(self._tlsConnection.open && self._tlsConnection.handshaking) {
+  this._socket.on('close', function(had_err) {
+    if(self.ssl.open && self.ssl.handshaking) {
       self.emit('error', new Error('Connection closed during handshake.'));
     }
 
-    self._tlsConnection.close();
+    self.ssl.close();
 
     // call socket handler
     self.emit('close', had_err);
   });
 
   // handle error on socket
-  self._socket.on('error', function(e) {
-    options.debug && self._trace('Socket error: ' + (e.message || e));
+  this._socket.on('error', function(e) {
+    debug('Socket error: ' + (e.message || e));
 
     // error
     self.emit('error', e);
   });
 
   // handle receiving raw TLS data from socket
-  self._socket.on('data', function(data) {
+  this._socket.on('data', function(data) {
     var bytes = data.toString('binary');
-    options.debug && self._trace(bytes, SERVER_TO_CLIENT, 'enc');
+    debug(bytes, SERVER_TO_CLIENT, 'enc');
 
-    self._tlsConnection.process(bytes);
+    self.ssl.process(bytes);
   });
+
+  if (!this._socket)
+    // handle doing handshake after connecting
+    this._socket.once('connect', this._init.bind(this, ctx));
+  else
+    this._init(ctx);
+};
+util.inherits(TLSSocket, EventEmitter);
+exports.TLSSocket = TLSSocket;
+
+TLSSocket.prototype._init = function(ctx) {
+  debug('Socket connected. Handshaking...');
+  this.ssl.handshake(ctx.session.id);
+};
+
+TLSSocket.prototype.serializeStanza = function (el, cb) {
+    return cb(el.toString());
+};
+
+ /**
+  * Determines if the socket is connected or not.
+  *
+  * @return true if connected, false if not.
+  */
+TLSSocket.prototype.isConnected = function() {
+  return this.ssl.isConnected;
+};
 
   /**
    * Destroys this socket.
    */
-  self.destroy = function() {
-    self._socket.destroy();
-  };
+TLSSocket.prototype.destroy = function() {
+  this._socket.destroy();
+};
 
   /**
    * Connects this socket.
    */
-  self.connect = function(port, host) {
-    options.debug && self._trace('Connecting to ' + host + ':' + port);
-
-    self._socket.connect(port, host);
-  };
+TLSSocket.prototype.connect = function(port, host) {
+  debug('Connecting to ' + host + ':' + port);
+  this._socket.connect(port, host);
+};
 
   /**
    * Closes this socket.
    */
-  self.close = function() {
-    options.debug && self._trace('Closing connection');
-
-    self._tlsConnection.close();
-  };
-
+TLSSocket.prototype.close = function() {
+  debug('Closing connection');
+  this.ssl.close();
+};
 
   /**
    * Close this socket.
    * @type {Function}
    */
-  self.end = self.close;
-
-
-  /**
-   * Determines if the socket is connected or not.
-   *
-   * @return true if connected, false if not.
-   */
-  self.isConnected = function() {
-    return self._tlsConnection.isConnected;
-  };
+TLSSocket.prototype.end = TLSSocket.prototype.close;
 
   /**
    * Writes bytes to this socket.
@@ -160,39 +163,64 @@ var TlsSocket = function(options) {
    *
    * @return true on success, false on failure.
    */
-  self.write = function(bytes) {
-    options.debug && self._trace(bytes, CLIENT_TO_SERVER);
-
-    return self._tlsConnection.prepare(bytes);
-  };
-
-
-  self._trace = function(msg, direction, contentType) {
-    switch (direction) {
-      case CLIENT_TO_SERVER:
-        direction = '{c -> S}: ';
-        break;
-      case SERVER_TO_CLIENT:
-        direction = '{S -> c}: ';
-        break;
-      default:
-        direction = '';
-    }
-    msg = ('enc' !== contentType ? msg : '(enc) ' + forge.util.bytesToHex(msg));
-    console.log('[TLS Socket] ' + direction + msg);
-  }
+TLSSocket.prototype.write = function(bytes) {
+  debug(bytes, CLIENT_TO_SERVER);
+  return this.ssl.prepare(bytes);
 };
-TlsSocket.prototype = Object.create(EventEmitter.prototype);
 
 
-module.exports = {
-  connect: function(options, onconnect) {
-    // TODO: remove this later
-    options.debug = true;
+TLSSocket.prototype.getCipher = function(bytes) {
+  debug(bytes, CLIENT_TO_SERVER);
+  return this.ssl.prepare(bytes);
+};
 
-    var tlsSocket = new TlsSocket(options);
-    tlsSocket.on('connected', onconnect);
-    return tlsSocket;
+function onConnected(conn) {
+  debug('Handshake successful');
+  // first handshake complete, call handler
+  if(conn.handshakes === 1) {
+    this.writable = true;
+    this.authorized = true;
+    this.emit('secureConnect');
   }
+}
+
+function onTlsDataReady(conn) {
+  var bytes = conn.tlsData.getBytes();
+  debug(bytes, CLIENT_TO_SERVER, 'enc');
+  // send TLS data over socket
+  this._socket.write(bytes, 'binary', function(err) {
+    if (err) {
+      this.emit('error', err);
+    }
+  }.bind(this));
+}
+
+function onDataReady(conn) {
+  var received = conn.data.getBytes();
+  debug(received, SERVER_TO_CLIENT, 'plain');
+  // indicate application data is ready
+  this.emit('data', new Buffer(received, 'binary'));
+}
+
+function onClosed() {
+  this.writable = false;
+  this.authorized = false;
+}
+
+function onError(conn, e) {
+  debug('Error: ' + e.message || e);
+  // send error, close socket
+  this.authorizationError = e; // FIXME probably not right
+  this.emit('error', e);
+  this._socket.end();
+}
+
+
+exports.connect = function(options, onconnect) {
+  var socket = new TLSSocket(options);
+  if (onconnect) {
+    socket.on('secureConnect', onconnect);
+  }
+  return socket;
 };
 
